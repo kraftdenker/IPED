55a8ed4816ac0d6b6e1812bfc50482018e234193
kraftdenker <alberto.magno@gmail.com>
2025-06-26 09:20:23 -0300

    It is a proposition to add a new feature to the IPED project. The feature will allow users to fast visualize audio metadata and also hear to audio to verify the accuracy of the transcriptions. User can just select audio artifact and press space bar to play audio. Maybe the embended VLC player can be used also for other media files fast visualization.  It uses project https://github.com/caprica/vlcj  provided under the GPL, version 3 or later.  And also portable VLC binaries from https://www.videolan.org/vlc/
    I guess vlc binaries can be striped to just the mais DLL (libvlc.dll and libvlccore.dll) and plugins dir.
    This feature will be useful for forensic investigators and digital forensics experts who need to quickly assess audio files during investigations.

--- iped-viewers/iped-viewers-impl/src/main/java/iped/viewers/AudioViewer.java
+++ iped-viewers/iped-viewers-impl/src/main/java/iped/viewers/AudioViewer.java
@@ -1,260 +1,285 @@
 package iped.viewers;
 
-import java.awt.Color;
+import java.awt.BorderLayout;
+import java.awt.Button;
+import java.awt.Component;
 import java.awt.GridLayout;
-import java.awt.image.BufferedImage;
-import java.io.ByteArrayOutputStream;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.Field;
+import java.net.URI;
 import java.nio.file.Files;
-import java.nio.file.StandardOpenOption;
-import java.util.Base64;
+import java.nio.file.StandardCopyOption;
+import java.util.Map;
 import java.util.Set;
 
-import javax.imageio.ImageIO;
-import javax.swing.UIManager;
+import javax.swing.AbstractAction;
+import javax.swing.Action;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import iped.data.IItemReader;
 import iped.io.IStreamSource;
-import iped.properties.BasicProps;
-import iped.properties.ExtraProperties;
-import iped.utils.IconUtil;
-import iped.utils.SimpleHTMLEncoder;
-import iped.utils.UiUtil;
+import iped.io.URLUtil;
 import iped.viewers.api.AbstractViewer;
-import iped.viewers.api.AttachmentSearcher;
-import iped.viewers.localization.Messages;
-import javafx.application.Platform;
-import javafx.embed.swing.JFXPanel;
-import javafx.scene.Scene;
-import javafx.scene.layout.StackPane;
-import javafx.scene.web.WebEngine;
+import uk.co.caprica.vlcj.player.base.MediaApi;
+import uk.co.caprica.vlcj.player.component.EmbeddedMediaPlayerComponent;
+import uk.co.caprica.vlcj.player.embedded.EmbeddedMediaPlayer;
 
 public class AudioViewer extends AbstractViewer {
-    private static final String transcriptionAttr = ExtraProperties.TRANSCRIPT_ATTR;
-    private static final String transcriptionConfidenceAttr = ExtraProperties.CONFIDENCE_ATTR;;
-    private static final String audioDurationAttr = ExtraProperties.AUDIO_META_PREFIX + "xmpDM:duration";
 
-    private JFXPanel jfxPanel;
-    private HtmlLinkViewer htmlViewer;
-    private String playImgBase64 = "";
+	@SuppressWarnings("unused")
+	private static Logger LOGGER = LoggerFactory.getLogger(AudioViewer.class);
+	private MediaApi player;
+	private EmbeddedMediaPlayer mediaPlayerComponent;
+	private JTextArea audioDescription = new JTextArea();
+	// private ProgressBar progressBar = new ProgressBar(0d);
+	private Button play = new Button(">");
+	private JButton playButton;
+	private JButton pauseButton;
+	private JButton rewindButton;
+	private volatile File temp;
+    public static void criarVlcjConfig(String pathVlc) {
+        String appData = System.getenv("APPDATA");
+        if (appData == null) {
+            System.err.println("Erro: variável de ambiente APPDATA não encontrada.");
+            return;
+        }
 
-    public AudioViewer(AttachmentSearcher attachmentSearcher) {
-        super(new GridLayout());
-        jfxPanel = new JFXPanel();
-        htmlViewer = new HtmlLinkViewer(attachmentSearcher);
+        File vlcjDir = new File(appData, "vlcj");
+        if (!vlcjDir.exists()) {
+            boolean criado = vlcjDir.mkdirs();
+            if (!criado) {
+                System.err.println("Erro ao criar diretório: " + vlcjDir.getAbsolutePath());
+                return;
+            }
+        }
 
-        Platform.runLater(new Runnable() {
+        File configFile = new File(vlcjDir, "vlcj.config");
+        if (configFile.exists()) {
+            System.out.println("Arquivo vlcj.config já existe em: " + configFile.getAbsolutePath());
+            return;
+        }
+
+        try (FileWriter writer = new FileWriter(configFile)) {
+            writer.write("nativeDirectory=" + pathVlc + System.lineSeparator());
+            System.out.println("Arquivo vlcj.config criado com sucesso em: " + configFile.getAbsolutePath());
+        } catch (IOException e) {
+            System.err.println("Erro ao criar arquivo vlcj.config: " + e.getMessage());
+        }
+    }
+	public AudioViewer()  {
+		super(new GridLayout());
+        if (System.getProperty("os.name").startsWith("Windows")) {
+			try {
+			URI jarUri = URLUtil.getURL(AudioViewer.class).toURI();
+            File nativelib = new File(jarUri).getParentFile();
+
+			System.setProperty("VLC_PLUGIN_PATH",
+                new File(nativelib, "\\vlc").getAbsolutePath());
+			
+				//File pluginsvlc = new File(nativelib, "nativelibs/vlc/plugins");
+			//setEnv("VLC_PLUGIN_PATH", pluginsvlc.getAbsolutePath());
+			File vlc = new File(System.getProperty("VLC_PLUGIN_PATH"), "\\win32-x86-64");
+			criarVlcjConfig(vlc.getAbsolutePath());
+			System.setProperty("jna.library.path", vlc.getAbsolutePath());
+			
+			//File libvlc = new File(nativelib, "nativelibs/vlc/libvlc.dll");
+			//System.load(libvlc.getAbsolutePath());
+
+			/*System.setProperty("java.library.path", System.getProperty("java.library.path") + ";" + vlc.getAbsolutePath());
+			Field fieldSysPath = ClassLoader.class.getDeclaredField("sys_paths");
+        	fieldSysPath.setAccessible(true);
+        	fieldSysPath.set(null, null);*/
+			//File libvlc = new File(nativelib, "nativelibs/vlc/libvlc.dll");
+            //File libvlccore = new File(nativelib, "nativelibs/vlc/libvlccore.dll");
+            //File axvlc = new File(nativelib, "nativelibs/vlc/axvlc.dll");
+            //File npvlc = new File(nativelib, "nativelibs/vlc/npvlc.dll");
+			//File pluginsvlc = new File(nativelib, "nativelibs/vlc/plugins");
+			//System.load(pluginsvlc.getAbsolutePath());
+            //System.load(npvlc.getAbsolutePath());
+            //System.load(axvlc.getAbsolutePath());
+            //System.load(libvlccore.getAbsolutePath());
+            //System.load(libvlc.getAbsolutePath());
+			} catch (Exception e) {
+				LOGGER.error("Error loading VLC native libraries", e);
+			}
+        }
+
+
+	}
+
+	@Override
+	public String getName() {
+		return "Audio"; //$NON-NLS-1$
+	}
+
+	@Override
+	public boolean isSupportedType(String contentType) {
+
+		return contentType.equals("audio/basic") || contentType.equals("audio/basic") ||
+		// contentType.equals("auido/L24")||
+		// contentType.equals("audio/mid")||
+		// contentType.equals("audio/mid")||
+				contentType.equals("audio/mpeg") || contentType.equals("audio/mp4")
+				|| contentType.equals("audio/x-aiff") || contentType.equals("audio/x-aiff")
+				|| contentType.equals("audio/x-aiff") || contentType.equals("audio/x-mpegurl")
+				|| contentType.equals("audio/vnd.rn-realaudio") || contentType.equals("audio/vnd.rn-realaudio")
+				|| contentType.equals("audio/ogg") || contentType.equals("audio/vorbis")
+				|| contentType.equals("audio/vnd.wave");
+
+	}
+
+	@Override
+	public void loadFile(IStreamSource content, Set<String> highlightTerms) {
+		if (temp != null) {
+			temp.delete();
+		}
+		if (player != null) {
+			player = null;
+		}
+		play.setLabel(">");
+		play.requestFocus();
+		audioDescription.setText("");
+		// progressBar.setProgress(0d);
+
+		// create temp file here to not block EDT
+		if (content != null) {
+			try (InputStream in = content.getSeekableInputStream()) {
+				// put file in temp
+				temp = File.createTempFile("IPED", ".audio", null);
+				temp.deleteOnExit();
+				Files.copy(in, temp.toPath(), StandardCopyOption.REPLACE_EXISTING);
+
+				boolean ok = mediaPlayerComponent.media().prepare(temp.getAbsolutePath());
+				if (ok) player = mediaPlayerComponent.media();
+				else
+					LOGGER.info("Error open file");
+				/*
+				 * HeadlessMediaPlayer mediaPlayer =
+				 * mediaPlayerFactory.newHeadlessMediaPlayer();
+				 * mediaPlayer.playMedia("path/to/your/file.ogg"); mediaPlayer.
+				 */
+
+				/*
+				 * Media mediaFile = new Media(temp.toURI().toString()); player = new
+				 * MediaPlayer(mediaFile); player.stop();
+				 * player.currentTimeProperty().addListener((observable, oldValue, newValue) ->
+				 * { double progress = newValue.toMillis() /
+				 * player.getTotalDuration().toMillis(); progressBar.setProgress(progress); });
+				 */
+				iped.data.IItem item = (iped.data.IItem) content;
+
+				for (String mdName : item.getMetadata().names()) {
+					if (mdName.contains("audio")
+							&& (mdName.contains("dura") || mdName.contains("transc") || mdName.contains("name"))) {
+						String mdValue = item.getMetadata().get(mdName);
+						audioDescription.append(mdName + ":" + mdValue + "\n\n");
+					}
+				}
+
+			} catch (IOException e) {
+				e.printStackTrace();
+				temp = null;
+			}
+		}
+	}
+	@SuppressWarnings("unchecked")
+	public static void setEnv(String key, String value) throws Exception {
+		Map<String, String> env = System.getenv();
+		Class<?> cl = env.getClass();
+		Field field = cl.getDeclaredField("m");
+		field.setAccessible(true);
+		Map<String, String> writableEnv = (Map<String, String>) field.get(env);
+		writableEnv.put(key, value);
+	}
+
+	@Override
+	public void init() {
+		
+
+		EmbeddedMediaPlayerComponent playerCompo = new EmbeddedMediaPlayerComponent();
+		mediaPlayerComponent = playerCompo.mediaPlayer();
+
+		this.getPanel().add(playerCompo);
+		this.getPanel().add(play);
+		this.getPanel().setLayout(new BorderLayout());
+		this.getPanel().add(playerCompo, BorderLayout.CENTER);
+		JPanel controlsPane = new JPanel();
+        
+        Action keyPAction = new AbstractAction() {
             @Override
-            public void run() {
-                StackPane root = new StackPane();
-                root.getChildren().add(htmlViewer.htmlViewer);
-                Scene scene = new Scene(root);
-                jfxPanel.setScene(scene);
+            public void actionPerformed(ActionEvent e) {
+                mediaPlayerComponent.controls().play();
             }
-        });
+        };
+        controlsPane.getInputMap(JPanel.WHEN_IN_FOCUSED_WINDOW).put(javax.swing.KeyStroke.getKeyStroke("SPACE"), "play");
+        controlsPane.getActionMap().put("play", keyPAction);
 
-        this.getPanel().add(jfxPanel);
-    }
+		playButton = new JButton("Play");
+		controlsPane.add(playButton);
+		pauseButton = new JButton("Pause");
+		controlsPane.add(pauseButton);
+		rewindButton = new JButton("Rewind");
+		controlsPane.add(rewindButton);
+		//skipButton = new JButton("Skip");
+		//controlsPane.add(skipButton);
+		this.getPanel().add(controlsPane, BorderLayout.SOUTH);
 
-    @Override
-    public boolean isSupportedType(String contentType) {
-        return contentType.startsWith("audio/");
-    }
+		playButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				mediaPlayerComponent.controls().play();
+			}
+		});
+		pauseButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				mediaPlayerComponent.controls().pause();
+			}
+		});
+		rewindButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				mediaPlayerComponent.controls().skipTime(-14000);
+			}
+		});
+		/*skipButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				mediaPlayerComponent.mediaPlayer().controls().skipTime(4000);
+			}
+		});*/
+		audioDescription = new JTextArea(3, 1);
+		audioDescription.setLineWrap(true);
+		audioDescription.setWrapStyleWord(true);
+		JScrollPane scrollPane = new JScrollPane(audioDescription, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
+				JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
 
-    @Override
-    public String getName() {
-        return "Audio";
-    }
+		audioDescription.setEditable(false);
+		this.getPanel().add(scrollPane);
+		// mediaPlayerFactory = new MediaPlayerFactory();
+	}
 
-    @Override
-    public void init() {
-        htmlViewer.init();
-        try {
-            BufferedImage img = ImageIO.read(IconUtil.class.getResource(resPath + "play.png"));
-            ByteArrayOutputStream out = new ByteArrayOutputStream(4096);
-            ImageIO.write(img, "png", out);
-            playImgBase64 = Base64.getEncoder().encodeToString(out.toByteArray());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-     }
+	@Override
+	public void copyScreen(Component comp) {
+	}
 
-    @Override
-    public void dispose() {
-        htmlViewer.dispose();
-    }
+	@Override
+	public void dispose() {
+	
+		  if (mediaPlayerComponent != null) { 
+			  mediaPlayerComponent.controls().stop();
+		  }
+		 
+	}
 
-    @Override
-    public void loadFile(final IStreamSource content, final Set<String> terms) {
-        loadFile(content, null, terms);
-    }
+	@Override
+	public void scrollToNextHit(boolean forward) {
+	}
 
-    @Override
-    public void loadFile(final IStreamSource content, String contentType, final Set<String> terms) {
-        Platform.runLater(new Runnable() {
-            @Override
-            public void run() {
-                WebEngine webEngine = htmlViewer.webEngine;
-                webEngine.loadContent(UiUtil.getUIEmptyHtml());
 
-                if (content instanceof IItemReader) {
-                    htmlViewer.highlightTerms = terms;
-                    String preview = generatePreview((IItemReader) content);
-                    try {
-                        if (htmlViewer.tmpFile == null) {
-                            htmlViewer.tmpFile = File.createTempFile("transcription", ".html");
-                            htmlViewer.tmpFile.deleteOnExit();
-                        }
-                        Files.write(htmlViewer.tmpFile.toPath(), preview.getBytes("UTF-8"),
-                                StandardOpenOption.TRUNCATE_EXISTING);
-                        webEngine.load(htmlViewer.tmpFile.toURI().toURL().toString());
-
-                    } catch (IOException e) {
-                        webEngine.loadContent(preview);
-                    }
-                }
-            }
-        });
-    }
-
-    private String generatePreview(IItemReader item) {
-        String transcription = item.getMetadata().get(transcriptionAttr);
-
-        String strConfidence = null;
-        if (transcription != null) {
-            String conf = item.getMetadata().get(transcriptionConfidenceAttr);
-            if (conf != null && !conf.isBlank()) {
-                try {
-                    double c = Double.parseDouble(conf);
-                    strConfidence = String.format("%.0f%%", c * 100);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-
-        String strDuration = null;
-        String duration = item.getMetadata().get(audioDurationAttr);
-        if (duration != null && !duration.isBlank()) {
-            try {
-                double d = Double.parseDouble(duration);
-                int seconds = (int) d;
-                if (seconds > 0) {
-                    int h = seconds / 3600;
-                    int m = seconds % 3600 / 60;
-                    int s = seconds % 60;
-                    if (h > 0) {
-                        strDuration = String.format("%dh %dm %ds", h, m, s);
-                    } else if (m > 0) {
-                        strDuration = String.format("%dm %ds", m, s);
-                    } else {
-                        strDuration = String.format("%ds", s);
-                    }
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-
-        String type = item.getType();
-
-        Color color2 = new Color(0xD0D0D0);
-        Color color3 = new Color(0xF4F4F4);
-        Color background = UIManager.getColor("Viewer.background");
-        if (background != null) {
-            color3 = UiUtil.mix(background, Color.gray, 0.9);
-            color2 = UiUtil.mix(background, Color.gray, 0.7);
-        }
-        String borderColor = "black";
-        Color foreground = UIManager.getColor("Viewer.foreground");
-        if (foreground != null && background != null)
-            borderColor = UiUtil.getHexRGB(UiUtil.mix(background, foreground, 0.5));
-
-        StringBuilder sb = new StringBuilder();
-        sb.append("<!DOCTYPE html>\n");
-        sb.append("<html>\n");
-        sb.append("<head>\n");
-        sb.append("<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\" />\n");
-        sb.append(
-                "<style>table {border-collapse: collapse; font-size:10pt; font-family: arial, verdana, sans-serif; width:100%; align:center; } ");
-        sb.append("table.t {margin-bottom:20px;} td { padding: 2px; } ");
-        sb.append("td.s1 {background-color:").append(UiUtil.getHexRGB(color2)).append("; ");
-        sb.append("border: 1px solid ").append(borderColor).append("; text-align:left;} ");
-        sb.append("td.s2 {background-color:").append(UiUtil.getHexRGB(color3)).append("; ");
-        sb.append("border: 1px solid ").append(borderColor).append("; text-align:left;}\n");
-        sb.append("textarea {readonly: readonly; height: 60px; width: 100%; resize: none;}\n");
-        sb.append("*:focus {outline: none;}\n");
-        sb.append("a.play {\n");
-        sb.append("  display: inline-block;\n");
-        sb.append("  vertical-align: top;\n");
-        sb.append("  cursor: pointer;\n");
-        sb.append("  width: 26px;\n");
-        sb.append("  height: 26px;\n");
-        sb.append("  margin: 3px;\n");
-        sb.append("  background-image: url(\"data:image/png;base64,").append(playImgBase64).append("\");\n");
-        sb.append("  background-position: 0px 0px;\n");
-        sb.append("  background-size: 26px 52px;\n");
-        sb.append("}\n");
-        sb.append("a.play:hover {\n");
-        sb.append("  background-position: 0px -26px;\n");
-        sb.append("}\n");
-        sb.append("</style></head>\n");
-        sb.append("<body style=\"");
-
-        if (background != null)
-            sb.append("background-color:").append(UiUtil.getHexRGB(background)).append(";");
-        if (foreground != null)
-            sb.append("color:").append(UiUtil.getHexRGB(foreground)).append(";");
-        sb.append("\">\n");
-
-        sb.append("<table class=\"t\">");
-        sb.append("<tr>");
-
-        sb.append("<td class=\"s1\" width=\"26px\">");
-        sb.append("<a class=\"play\" onclick=\"app.open('");
-        if (item.getHash() != null) {
-            sb.append(BasicProps.HASH).append(':').append(item.getHash());
-        }
-        sb.append("')\"/>");
-        sb.append("</td>");
-
-        sb.append("<td class=\"s1\">");
-        sb.append("<b>").append(SimpleHTMLEncoder.htmlEncode(Messages.getString("AudioViewer.TranscriptionTitle"))).append("</b>");
-        if (strConfidence != null) {
-            sb.append(" [").append(strConfidence).append("]");
-        }
-        if (strDuration != null || type != null) {
-            sb.append("<br>");
-            if (strDuration != null) {
-                sb.append(strDuration).append(' ');
-            }
-            if (type != null) {
-                sb.append('(').append(type).append(')');
-            }
-        }
-        sb.append("</td></tr>");
-
-        sb.append("<tr>");
-        sb.append("<td colspan=\"2\" class=\"s2\">");
-        if (transcription == null) {
-            sb.append("<font color=\"gray\"> [");
-            sb.append(SimpleHTMLEncoder.htmlEncode(Messages.getString("AudioViewer.NoTranscription")));
-            sb.append("] </font>");
-        } else {
-            sb.append(SimpleHTMLEncoder.htmlEncode(transcription));
-        }
-        sb.append("</td></tr>");
-
-        sb.append("</table>");
-        sb.append("</body>");
-        sb.append("</html>");
-
-        return sb.toString();
-    }
-
-    @Override
-    public void scrollToNextHit(boolean forward) {
-        htmlViewer.scrollToNextHit(forward);
-    }
 }
